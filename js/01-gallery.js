cd1f7858ac480b8a539a0651941c1265cc61a310
itsjenm <jmartinez414@gmail.com>
2023-01-06 17:56:24 -0600

    updates
    changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryGrid = document.querySelector(".gallery");
+const galleryImage = document.querySelector(".gallery_item");
 
+for (let i = 0; i < 9; i++ ) {
+    const list = document.createElement('div');
+    list.classList
+}
 console.log(galleryItems);
