8311d042251b8da815253fcc50e2feeace9c3707
itsjenm <jmartinez414@gmail.com>
2023-01-07 01:09:22 -0600

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,30 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 const galleryGrid = document.querySelector(".gallery");
-const galleryImage = document.querySelector(".gallery_item");
 
-for (let i = 0; i < 9; i++ ) {
-    const list = document.createElement('div');
-    list.classList
-}
+
+const markup = galleryItems.map((image) => `<div class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+        <img 
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+        />
+    </a>
+</div>`).join("");
+
+console.log(markup)
+
+galleryGrid.innerHTML = markup;
+
+
+galleryGrid.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(event.target.dataset.source);
+    basicLightbox.create(`
+    <img width="1400" height="900" src="${event.target.dataset.source}">`).show()
+});
+
 console.log(galleryItems);
